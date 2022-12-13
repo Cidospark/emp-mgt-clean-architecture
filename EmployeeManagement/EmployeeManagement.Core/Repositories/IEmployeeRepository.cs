9443cfaa311e071c9f2a9754b4ad3ac4158ea0d3
Cidospark <cidospark27@gmail.com>
2022-12-13 09:48:57 +0100

    Entities and Repositories added for core project

---
+++ EmployeeManagement/EmployeeManagement.Core/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Employee.Core.Repositories.Base;
+
+namespace Employee.Core.Repositories
+{
+    public interface IEmployeeRepository : IRepository<Employee.Core.Entities.Employee>
+    {
+        Task<IEnumerable<Employee.Core.Entities.Employee>> GetEmployeeByLastName(string lastName);
+    }
+}
+
